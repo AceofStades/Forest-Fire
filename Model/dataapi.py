2dd44a518581099197fb16c35b4da18d614953e6
AceofStades <rahulpbokade@gmail.com>
2025-07-31 01:28:02 +0530

    Initial Commit

---
+++ Model/dataapi.py
@@ -0,0 +1,53 @@
+
+import cdsapi
+
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": ["reanalysis"],
+    "variable": [
+        "total_cloud_cover",
+        "evaporation",
+        "total_precipitation",
+        "soil_temperature_level_1",
+        "soil_type",
+        "volumetric_soil_water_layer_1",
+        "high_vegetation_cover",
+        "low_vegetation_cover",
+        "type_of_high_vegetation",
+        "type_of_low_vegetation",
+        "10m_u_component_of_wind",
+        "10m_v_component_of_wind",
+        "2m_dewpoint_temperature",
+        "2m_temperature"
+    ],
+    "year": ["2021"],
+    "month": ["03", "04"],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "data_format": "netcdf",
+    "download_format": "zip",
+    "area": [37, 68, 8, 97]
+}
+
+client = cdsapi.Client()
+client.retrieve(dataset, request).download()
