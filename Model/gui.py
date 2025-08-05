f4634b4642edb4711564334f17d0447db5cc32e4
AceofStades <rahulpbokade@gmail.com>
2025-08-05 21:53:41 +0530

    Remove old dataset and view new dataset

--- Model/gui.py
+++ Model/gui.py
@@ -2,24 +2,24 @@ import streamlit as st
 import xarray as xr
 # from netCDF4 import Dataset
 
-# file = Dataset("dataset/data_stream-oper_stepType-accum.nc")
-ds1 = xr.open_dataset("dataset/data_stream-oper_stepType-accum.nc")
+st.set_page_config(layout="wide")
+
+ds1 = xr.open_dataset("dataset/ERA5-Land/era5-april/cvl.area-subset.31.5.81.1.28.7.77.5.nc")
 df1 = ds1.to_dataframe()
 
-ds2 = xr.open_dataset("dataset/data_stream-oper_stepType-instant.nc")
+ds2 = xr.open_dataset("dataset/ERA5-Land/era5-april/data_0.nc")
 df2 = ds2.to_dataframe()
 
-vbds1 = xr.open_dataset("vishal-datasets/merged_era5_2015_2016.nc")
-vbdf1 = vbds1.to_dataframe()
+ds3 = xr.open_dataset("dataset/ERA5-Land/era5-may/cvl.area-subset.31.5.81.1.28.7.77.5.nc")
+df3 = ds3.to_dataframe()
 
-vbds2 = xr.open_dataset("vishal-datasets/viirs_binary_fire_2015_2016.nc")
-vbdf2 = vbds2.to_dataframe()
+ds4 = xr.open_dataset("dataset/ERA5-Land/era5-may/data_0.nc")
+df4 = ds4.to_dataframe()
 
-# print(df.head())
-# st.dataframe(df1.head())
-# st.dataframe(df2.head())
+# st.dataframe(df1)
+st.dataframe(df1.head())
+st.dataframe(df2.head())
+st.dataframe(df3.head())
+st.dataframe(df4.head())
 
-# st.dataframe(vbdf1.head())
-# st.dataframe(vbdf2.head())
-
-st.dataframe(df2)
+# st.dataframe(df2)
