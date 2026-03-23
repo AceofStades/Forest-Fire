baff322a6b552bfd9bafb68239d8f049096a3699
AceofStades <rahulpbokade@gmail.com>
2026-03-23 17:54:54 +0530

    v0.5.6: The GREAT refactor

--- Model/training/custom_dataset/inspect_weights.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-path = "Model/checkouts/best_unet.pth"
-state_dict = torch.load(path, map_location="cpu", weights_only=True)
-print("Bias in saved state dict:")
-print(state_dict["outc.bias"])
-
-print("Conv weights max:")
-print(state_dict["outc.weight"].max())
